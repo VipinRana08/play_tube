fe0cbff4f5a7bddc83ff978a55089140d41851cf
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2024-09-24 00:50:08 +0530

    Bug fix for comment endpoints

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,6 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import {Comment} from "../models/comment.model.js";
-import {ApiError} from "../utils/ApiErrors.js";
+import {ApiErrors} from "../utils/ApiErrors.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
 
@@ -9,7 +9,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {page = 1, limit = 10} = req.query;
 
     if(!isValidObjectId(videoId)){
-        throw new ApiError(400, "Invalid video ID");
+        throw new ApiErrors(400, "Invalid video ID");
     }
 
     const paginationOptions = {
@@ -22,16 +22,22 @@ const getVideoComments = asyncHandler(async (req, res) => {
     };
     const pipeline = [
         {
-            $match: { video: videoId } 
+            $match: { video: new mongoose.Types.ObjectId(videoId) }
+        },
+        {
+            $project: {
+                content: 1,
+                owner: 1,  
+                createdAt: 1
+            }
         }
     ];
     const comments = await Comment.aggregatePaginate(
         Comment.aggregate(pipeline),
         paginationOptions
     );
-
-    if(!comment){
-        throw new ApiError(404, "No comments Found! 😢");
+    if(!comments){
+        throw new ApiErrors(404, "No comments Found! 😢");
     }
 
     return res.status(200).json(new ApiResponse(
@@ -46,10 +52,10 @@ const addComment = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
 
     if(!isValidObjectId(videoId)){
-        throw new ApiError(400, "Invalid video ID!");
+        throw new ApiErrors(400, "Invalid video ID!");
     }
     if (!content || content.trim() === "") {
-        throw new ApiError(400, "Field is required!");
+        throw new ApiErrors(400, "Field is required!");
     }
 
     const comment = await Comment.create({
@@ -70,10 +76,10 @@ const updateComment = asyncHandler(async (req, res) => {
     const { content } = req.body;
 
     if(!isValidObjectId(commentId)){
-        throw new ApiError(400, "Invalid comment ID!");
+        throw new ApiErrors(400, "Invalid comment ID!");
     }
     if (!content || content.trim() === "") {
-        throw new ApiError(400, "Field should not be empty!");
+        throw new ApiErrors(400, "Field should not be empty!");
     }
 
     const updatedComment = await Comment.findByIdAndUpdate(
@@ -83,7 +89,7 @@ const updateComment = asyncHandler(async (req, res) => {
     );
 
     if (!updatedComment) {
-        throw new ApiError(404, "Comment not found!");
+        throw new ApiErrors(404, "Comment not found!");
     }
     return res.status(200).json(new ApiResponse(
         200,
@@ -96,11 +102,11 @@ const deleteComment = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
 
     if(!isValidObjectId(commentId)){
-        throw new ApiError(400, "Invalid comment ID!");
+        throw new ApiErrors(400, "Invalid comment ID!");
     }
     const comment = await Comment.findById(commentId);
     if (!comment) {
-        throw new ApiError(404, "Comment not found!");
+        throw new ApiErrors(404, "Comment not found!");
     }
 
     await Comment.findByIdAndDelete(commentId);
--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -16,6 +16,6 @@ const commentSchema = new Schema({
     }
 }, {timestamps: true});
 
-videoSchema.plugin(mongooseAggregatePaginate);
+commentSchema.plugin(mongooseAggregatePaginate);
 
 export const Comment = mongoose.model("Comment", commentSchema);
